3fd30052872054a4c6cd13a2e3b2cfc508d59248
double0kung <00kung@gmail.com>
2024-09-09 13:09:12 +0000

    updated file 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # This program prints numbers from 0 to 99, separated by ', '.
-for num in range(100):
-    if num == 99:
-        print("{:02d}".format(num))
+for i in range(100):
+    if i == 99:
+        print("{:02d}".format(i))
     else:
-        print("{:02d}".format(num), end=", ")
+        print("{:02d}".format(i), end=", ")
