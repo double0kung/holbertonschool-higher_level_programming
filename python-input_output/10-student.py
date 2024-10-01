1755110eeed0339cd83ae903ab575c8a406bc65d
double0kung <00kung@gmail.com>
2024-10-01 07:54:15 +0000

    saving...

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -11,6 +11,10 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Retrieves a dictionary representation of a Student instance"""
-        return self.__dict__
+        if attrs is None:
+            return self.__dict__
+        return {attr: getattr(self, attr)
+                for attr in attrs
+                if hasattr(self, attr)}
